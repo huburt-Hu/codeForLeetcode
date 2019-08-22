da553c0d63e3a796dddf27668400424ccf52658a
huxibing <654360340@qq.com>
2019-08-22 10:18:22 +0800

    判断链表是否为回文链表；合并两个有序链表

--- src/main/com/huburt/list/ClassicQuestion.java
+++ src/main/com/huburt/list/ClassicQuestion.java
@@ -101,19 +101,80 @@ public class ClassicQuestion {
     }
 
 
+    /**
+     * 请判断一个链表是否为回文链表。
+     * <p>
+     * 示例 1:
+     * 输入: 1->2
+     * 输出: false
+     * 示例 2:
+     * 输入: 1->2->2->1
+     * 输出: true
+     * <p>
+     * 进阶：
+     * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+     */
+    public boolean isPalindrome(ListNode head) {
+        //找到中间node
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //反转后半段
+        ListNode reversed = reverseList(slow);
+        while (head != null && reversed != null) {
+            if (head.val != reversed.val) {
+                return false;
+            }
+            head = head.next;
+            reversed = reversed.next;
+        }
+        return true;
+    }
+
+    /**
+     * 合并两个有序链表
+     * <p>
+     * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     * <p>
+     * 示例：
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = null;
+        if (l1.val <= l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         ListNode n2 = new ListNode(2);
         head.next = n2;
         ListNode n3 = new ListNode(3);
         n2.next = n3;
-        ListNode n4 = new ListNode(4);
+        ListNode n4 = new ListNode(2);
         n3.next = n4;
-        n4.next = new ListNode(5);
+        n4.next = new ListNode(1);
         System.out.println(head.getList());
 
-        ListNode node = new ClassicQuestion().oddEvenList(head);
+        ClassicQuestion classicQuestion = new ClassicQuestion();
+//        ListNode node = classicQuestion.oddEvenList(head);
+//        System.out.println(node.getList());
 
-        System.out.println(node.getList());
+        boolean palindrome = classicQuestion.isPalindrome(head);
+        System.out.println(palindrome);
+
     }
 }
